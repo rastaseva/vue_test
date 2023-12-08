160b3cc5d0cc24667d4962425a206f63ed623049
rastaseva <rastaseva@gmail.com>
2023-12-08 18:08:08 +0300

    fixed error in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === '/vue_test/'
-      ? ''
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/vue_test/'
       : '/'
 })
